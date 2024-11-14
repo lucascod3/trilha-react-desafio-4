6322f517eed2d7f7fa5ec836bda48d9fd7338a08
lucascod3 <lukinha_matheus09@hotmail.com>
2024-11-14 02:00:45 -0300

    Update styles.ts

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,30 +1,28 @@
 import styled from 'styled-components';
 
-
-export const InputContainer = styled.div`
+export const InputWrapper = styled.div`
     width: 100%;
-    height: 42px;
-    color: #FFF;
+    height: 40px;
+    color: #FFFFFF;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
+    border: 1px solid #7D249C;
+    border-radius: 20px;
     overflow: hidden;
-    padding: 0 10px;
+    padding: 0 12px;
     
     & input {
         width: 100%;
-        height: 42px;
-        border-radius: 21px;
+        height: 40px;
+        border-radius: 20px;
         background-color: transparent;
-        border: 0;
+        border: none;
         outline: none;
     }
+`;
 
-`
-
-export const ErrorMessage = styled.p`
-    color: red;
-    font-size:12px;
-    margin-top:8px;
-    margin-left:10px;
-`
+export const ErrorText = styled.p`
+    color: blue;
+    font-size: 12px;
+    margin-top: 10px;
+    margin-left: 12px;
+`;
