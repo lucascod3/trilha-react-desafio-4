89e0f784803df4511beb90bac770f87bbf36dc21
lucascod3 <lukinha_matheus09@hotmail.com>
2024-11-14 01:55:01 -0300

    Update styles.ts

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
+import { IButtonProps } from './types';
 
-
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<IButtonProps>`
     width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+    height: 40px;
+    background-color: #7D249C;
+    color: #FFFFFF;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
+    border: 1px solid #7D249C;
+    border-radius: 20px;
 
     &:hover {
-        opacity: 0.6;
-        cursor:pointer;
+        opacity: 0.7;
+        cursor: pointer;
     }
-`
+`;
